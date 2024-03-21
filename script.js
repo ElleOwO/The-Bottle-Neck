3e65e3f483a3a3db7661c5af11f7d44e9efb3a39
ElleOwO <lmtdmya@gmail.com>
2024-03-20 18:30:13 -0600

    fixed forward and backwards and added a background fade animation

--- script.js
+++ script.js
@@ -2,39 +2,105 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-
+//param n: controls the css colors
 let slideIndex = 1;
+//call to show slide on page
 showSlides(slideIndex);
 
 // Next/previous controls
+//increments the slide index
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
+function minusSlides(n) {
+
+    showminusSlides(slideIndex += n);
+}
+
+// function minusSlides(n){
+//     showSlides(slideIndex -= n);
+// }
 
 // Thumbnail image controls
 function currentSlide(n) {
+    // if (n<1){
+    //     n=1;
+    // }
     showSlides(slideIndex = n);
 }
 
+
+
+
+
+
 function showSlides(n) {
     let i;
-    element = document.body;
+    let element = document.body;
 
+
+    //indicates the current css
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
+
+    //button clicked n state, if n greater than 1, index restarts
+
+    //
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        if (n==2){
-            element.classList.toggle("slide2");
 
+
+        slides[i].style.display = "none";
+        element.classList.add("slide" + slideIndex);
+        element.classList.remove("slide" + (n-1));
+        if (n<1){
+            for (i=0;i<slides.length; i++){
+                element.classList.remove("slide" + (slideIndex+1));
+
+            }
         }
+
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+    if (n > slides.length) {slideIndex = 1; n=1;
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    // JSON.stringify(element);
+    // console.info(element);
+    // console.log("n: " + n);
+    // console.log("i: " + i);
+    // console.log("slideindex:" + slideIndex)
     }
+
+function showminusSlides(n) {
+    let i;
+    let element = document.body;
+
+
+    //indicates the current css
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    //button clicked n state, if n greater than 1, index restarts
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    } else if (n > slides.length) {
+        slideIndex = 1;
+    }    for (i = 0; i < slides.length; i++) {
+
+
+        slides[i].style.display = "none";
+        element.classList.add("slide" + slideIndex);
+        element.classList.remove("slide" + (n + 1));
+    }
+    slides[slideIndex-1].style.display = "block";
+
+    // JSON.stringify(element);
+    // console.info(element);
+    // console.log("n: " + n);
+    // console.log("i: " + i);
+    // console.log("slideindex:" + slideIndex)
+
+}
 
